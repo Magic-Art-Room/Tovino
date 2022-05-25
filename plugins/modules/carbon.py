1f51f363c5027057b44fddc114325a5b9d61c728
Magic-Art-Room <99250685+Magic-Art-Room@users.noreply.github.com>
2022-05-25 21:31:38 +0530

    Update carbon.py

--- plugins/modules/carbon.py
+++ plugins/modules/carbon.py
@@ -6,13 +6,6 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 aiohttpsession = ClientSession()
 
 C = "Successfully Created Your Carbon"
-F = InlineKeyboardMarkup(
-[[
-     InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url="https://t.me/dk_botx")
-]]
-)
-
-
 
 
 @bot.on_message(filters.command("carbon"))
